c04476055379bccbadb3aae4582f490ac0d618d2
sanghee-dev <leeesangheee@gmail.com>
2021-10-20 01:25:08 +0900

    프로젝트 세팅

--- Filter Cam/MainViewController.swift
+++ Filter Cam/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Filter Cam
 //
 //  Created by leeesangheee on 2021/10/20.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
-
-
 }
-
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Filter Cam' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Filter Cam
+  pod 'RxCocoa'
+  pod 'RxDataSources'
+  pod 'RxSwift'
+
+end
